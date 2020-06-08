64387d380460a9a90c0bde11f195fe6337fa8a03
Ashley Schroeder <ashley.schroeder2016@gmail.com>
2020-06-08 21:54:39 +1200

    Set up world, hooks, and page objects

--- src/test/groovy/tmsandbox/stepdefs/HomeSteps.groovy
+++ src/test/groovy/tmsandbox/stepdefs/HomeSteps.groovy
@@ -1,49 +1,31 @@
 package tmsandbox.stepdefs
 
-import io.cucumber.java.After
+import io.cucumber.guice.ScenarioScoped
+import com.google.inject.Inject
 import io.cucumber.java.Before
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
-import io.github.bonigarcia.wdm.WebDriverManager
 import org.junit.Assert
-import org.junit.BeforeClass
-import org.openqa.selenium.By
-import org.openqa.selenium.WebDriver
-import org.openqa.selenium.WebElement
-import org.openqa.selenium.chrome.ChromeDriver
+import tmsandbox.pages.HomePage
 
+@ScenarioScoped
 class HomeSteps {
+    @Inject World world
 
-    private WebDriver driver
-
-//    @BeforeClass
-//    void setUpDriver() {
-//        WebDriverManager.chromedriver().setup()
-//    }
+    HomePage homePage
 
     @Before
-    void setUpWebDriver() {
-        WebDriverManager.chromedriver().setup()
-        driver = new ChromeDriver()
+    void setUpPages() {
+        homePage = new HomePage(world.driver)
     }
 
-    @After
-    void tearDown() {
-        if (driver != null) {
-            driver.manage().deleteAllCookies()
-            driver.quit()
-        }
+    @When('^I navigate to the (.+)$')
+    public void navigateToTradeMePage(page) {
+        world.driver.get(world.testProperties[page])
     }
 
-    @When('I navigate to the TradeMe homepage')
-    public void navigateToTradeMePage() {
-        driver.get('https://www.tmsandbox.co.nz/')
-    }
-
-    @Then('the homepage loads successfully')
+    @Then('^the homepage loads successfully$')
     public void validateHomePage() {
-        WebElement homepageLogo = driver.findElement(By.id('SiteHeader_SiteTabs_kevin'))
-
-        Assert.assertTrue(homepageLogo.isDisplayed())
+        Assert.assertTrue(homePage.homepageLogoDisplayed())
     }
 }
